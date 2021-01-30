1b2b5e03ffd91ceb6ccea083b47cda5a16404c62
Artur Dolinkin <dolart14@mail.ru>
2021-01-30 11:12:43 +0300

    refactor "brain-progression" game logic

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,34 +2,30 @@ import getRandomIntNum from '../getRandomIntNum.js';
 import runGameEngine from '../index.js';
 
 const getProgression = (firstElement, step, length) => {
-  const progression = [firstElement];
+  const progression = [];
 
-  for (let i = 1; i < length; i += 1) {
+  for (let i = 0; i < length; i += 1) {
     progression.push(firstElement + i * step);
   }
 
   return progression;
 };
 
-const getDataForGame = () => {
+const getGameData = () => {
   const progressionStep = getRandomIntNum(1, 31);
   const progressionFirstElement = getRandomIntNum(1, 71);
   const progressionLength = getRandomIntNum(5, 11);
   const progression = getProgression(progressionFirstElement, progressionStep, progressionLength);
 
   const missingElementIndex = getRandomIntNum(0, progressionLength);
-  const missingElement = progression[missingElementIndex];
+  const answer = progression[missingElementIndex].toString();
 
-  let progressionWithoutOneElement = '';
-  for (let i = 0; i < progressionLength; i += 1) {
-    if (i === missingElementIndex) {
-      progressionWithoutOneElement += '.. ';
-    } else {
-      progressionWithoutOneElement += `${progression[i]} `;
-    }
-  }
+  progression[missingElementIndex] = '..';
+  const question = progression.join(' ');
 
-  return [progressionWithoutOneElement, missingElement.toString()];
+  return [question, answer];
 };
 
-export default () => runGameEngine('What number is missing in the progression?', getDataForGame);
+const description = 'What number is missing in the progression?';
+
+export default () => runGameEngine(description, getGameData);
