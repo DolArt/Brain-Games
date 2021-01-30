1d76f63bdb63c771c1a4b4766f3648d9e1960dd2
Artur Dolinkin <dolart14@mail.ru>
2021-01-30 11:56:22 +0300

    Revert "refactor "brain-progression" game logic"
    This reverts commit 1b2b5e03ffd91ceb6ccea083b47cda5a16404c62.

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,30 +2,34 @@ import getRandomIntNum from '../getRandomIntNum.js';
 import runGameEngine from '../index.js';
 
 const getProgression = (firstElement, step, length) => {
-  const progression = [];
+  const progression = [firstElement];
 
-  for (let i = 0; i < length; i += 1) {
+  for (let i = 1; i < length; i += 1) {
     progression.push(firstElement + i * step);
   }
 
   return progression;
 };
 
-const getGameData = () => {
+const getDataForGame = () => {
   const progressionStep = getRandomIntNum(1, 31);
   const progressionFirstElement = getRandomIntNum(1, 71);
   const progressionLength = getRandomIntNum(5, 11);
   const progression = getProgression(progressionFirstElement, progressionStep, progressionLength);
 
   const missingElementIndex = getRandomIntNum(0, progressionLength);
-  const answer = progression[missingElementIndex].toString();
+  const missingElement = progression[missingElementIndex];
 
-  progression[missingElementIndex] = '..';
-  const question = progression.join(' ');
+  let progressionWithoutOneElement = '';
+  for (let i = 0; i < progressionLength; i += 1) {
+    if (i === missingElementIndex) {
+      progressionWithoutOneElement += '.. ';
+    } else {
+      progressionWithoutOneElement += `${progression[i]} `;
+    }
+  }
 
-  return [question, answer];
+  return [progressionWithoutOneElement, missingElement.toString()];
 };
 
-const description = 'What number is missing in the progression?';
-
-export default () => runGameEngine(description, getGameData);
+export default () => runGameEngine('What number is missing in the progression?', getDataForGame);
