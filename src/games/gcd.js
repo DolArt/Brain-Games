b1ca1df6d9d71ea7e5c00481151757c4303ab08f
Artur Dolinkin <dolart14@mail.ru>
2021-01-30 10:49:47 +0300

    refactor "brain-gcd" game logic

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,39 +1,28 @@
 import getRandomIntNum from '../getRandomIntNum.js';
 import runGameEngine from '../index.js';
 
-const getSortedPair = (number1, number2) => {
-  let numbers = [];
-
-  if (number1 < number2) {
-    numbers = [number1, number2];
-  } else {
-    numbers = [number2, number1];
+const getGCD = (num1, num2) => {
+  if (num1 === 0 || num2 === 0) {
+    return num1 + num2;
   }
 
-  return numbers;
-};
-
-const getGCD = (number1, number2) => {
-  const sortedNumbersPair = getSortedPair(number1, number2);
-  let gcd = 1;
-
-  for (let i = 2; i <= sortedNumbersPair[0]; i += 1) {
-    if (sortedNumbersPair[0] % i === 0 && sortedNumbersPair[1] % i === 0) {
-      gcd = i;
-    }
+  if (num1 > num2) {
+    return getGCD(num1 % num2, num2);
   }
 
-  return gcd;
+  return getGCD(num1, num2 % num1);
 };
 
-const getDataForGame = () => {
+const getGameData = () => {
   const firstNumber = getRandomIntNum(1, 100);
   const secondNumber = getRandomIntNum(1, 100);
 
   const question = `${firstNumber} ${secondNumber}`;
-  const answer = getGCD(firstNumber, secondNumber);
+  const answer = getGCD(firstNumber, secondNumber).toString();
 
-  return [question, answer.toString()];
+  return [question, answer];
 };
 
-export default () => runGameEngine('Find the greatest common divisor of given numbers.', getDataForGame);
+const description = 'Find the greatest common divisor of given numbers.';
+
+export default () => runGameEngine(description, getGameData);
